197f2ef2ed0db4d4c13a18a0dbe2e6c92c17ed68
Mehul <mehulshrivastava1011@gmail.com>
2022-11-09 09:35:10 +0530

    Starter files

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  // data binding property for view template (html)
+  title = 'Angular-starter';
+}
